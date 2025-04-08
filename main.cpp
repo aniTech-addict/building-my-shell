d867e5e66c486f1d17a7e969e4d8753f332208f0
Brad <kirti7666anand15@gmail.com>
2025-04-08 18:46:21 +0530

    REPL

--- main.cpp
+++ main.cpp
@@ -13,7 +13,8 @@ int main() {
 
   std::string input;
   std::getline(std::cin, input);
-  if(input == "exit 0") return 0;
+  if (!intput.compare("exit 0")) return 0;
+
   std::cout<<input<<": command not found"<<std::endl;
 }
 }
