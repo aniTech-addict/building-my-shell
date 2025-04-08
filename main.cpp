5506cc09e700682d4f651f4630a18bf1938a8cd2
Brad <kirti7666anand15@gmail.com>
2025-04-08 18:50:36 +0530

    echo

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ bool run = true;
 
 int main() {
   // Flush after every std::cout / std:cerr
-  while(run){
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+while(run){
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+    std::cout << "$ ";
 
+    std::string input;
+    std::getline(std::cin, input);
+    if (!input.compare("exit 0")) return 0;
+    if (!input.substr(0,5).compare("echo ")) std::cout << input.substr(5,input.length())<<std::endl;
 
-  std::string input;
-  std::getline(std::cin, input);
-  if (!intput.compare("exit 0")) return 0;
-
-  std::cout<<input<<": command not found"<<std::endl;
+    std::cout<<input<<": command not found"<<std::endl;
 }
 }
