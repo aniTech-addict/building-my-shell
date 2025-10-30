a33f0b99ae4c5080ad9a9e0602d7adf09c312efe
aniTech-addict <kirti7666anand15@gmail.com>
2025-10-30 15:25:22 +0530

    feat(shell): implement command execution capability

--- my_shell.cpp
+++ my_shell.cpp
@@ -16,9 +16,43 @@ int main(){
         cout<<"my_shell> ";
         getline(cin, input);
 
-        if(input == "exit"){
-            exit(0);
+        // Special commands
+        if(input.empty()) continue;
+        if(input == "exit") exit(0);
+
+
+        // parsing input into tokens
+        vector<string> tokens;
+        stringstream ss(input);
+        string token;
+        while (ss >> token) {
+            tokens.push_back(token);
         }
+
+        vector<char*> args;
+        for(auto& token : tokens){
+            args.push_back(&token[0]);
+        }
+        args.push_back(nullptr);
+
+        //child process
+        pid_t pid = fork();
+
+        if(pid < 0){
+            perror("fork");
+            exit(1);
+        }
+
+        if(pid == 0){
+            execvp(args[0],args.data()); // if err , execvp would return -1 else nothing and we would just jump 
+            cerr << "Command not found: " << args[0] << endl;
+            exit(1); 
+        }
+
+        int status;
+        waitpid(pid, &status, 0);
+
+
     }
 
     return 0;
