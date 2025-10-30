0fc37633d0604a500b1ea6b776448b8d041841c9
aniTech-addict <kirti7666anand15@gmail.com>
2025-10-30 10:49:18 +0530

    (delete) restarting from scratch

--- main.cpp
+++ main.cpp
@@ -7,27 +7,8 @@
 bool run = true;
 
 int main() {
-  // Flush after every std::cout / std:cerr
-while(run){
-    std::cout << std::unitbuf;
-    std::cerr << std::unitbuf;
-
-    std::cout << "$ ";
-
-    std::string input;
-    std::getline(std::cin, input);
-    if (!input.compare("exit 0")) return 0;
-    if (!input.substr(0,5).compare("echo ")) std::cout << input.substr(5,input.length())<<std::endl;
-    if (!input.substr(0,5).compare("type ")) 
-    {
-        std::string command = input.substr(5,input.length());
-        if (commands.find(command) != commands.end()) {
-            std::cout << command << ": " << commands[command] << std::endl;
-        } else {
-            std::cout << command << ": command not found" << std::endl;
-        }
+  
+    while(run){
+    
     }
-
-    else std::cout<<input<<": command not found"<<std::endl;
-}
 }
--- type.hpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <map>
-
-std::map <std::string , std::string> commands = {
-    {"echo", "builtin"},
-    {"exit", "builtin"},
-};
